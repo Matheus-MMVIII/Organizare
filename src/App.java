402a07d51c98b3763107680d929a18d909951428
Matheus Viturino <65409312+Matheus-MMVIII@users.noreply.github.com>
2026-03-18 17:03:38 +0000

    Create a SQL Server and minimal config

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class App {
+
     public static void main(String[] args) {
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado ao banco!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Users user1 = new Users(1, "John Doe", "john.doe@example.com", "123-456-7890", 5, 15);
         System.out.println("User: " + user1.getName());
         System.out.println("Email: " + user1.getEmail());
