bcfcff62cc672aa550b96c379edd0e1ac5bdc9cb
Matheus Viturino <65409312+Matheus-MMVIII@users.noreply.github.com>
2026-03-31 03:51:55 +0000

    Create Color enum.

---
+++ src/main/java/com/organizare/model/Color.java
@@ -0,0 +1,6 @@
+package com.organizare.model;
+
+public enum Color {
+    Branco, Preto, Cinza, Azul, Azul_marinho, Vermelho, Verde, Verde_oliva, Amarelo, Bege, Marrom, Caramelo, Rosa,
+    Rosa_claro, Roxo, Lilás, Laranja, Vinho, Turquesa, Ciano, Off_white, Nude, Creme, Dourado, Prateado
+}
